c64dc4e3c531388d345a30867fed1f806a25b3c5
Magnus Åsrud <magnusasrud92@gmail.com>
2016-05-22 04:56:32 +0200

    Faster loop
    Cheating

--- src/ex4.rs
+++ src/ex4.rs
@@ -1,8 +1,8 @@
 pub fn ex4() -> i32 {
 	// 906609
 	let mut result = 0;
-	for x in 1..1000 {
-		for y in 1..1000 {
+	for x in 100..1000 {
+		for y in x..1000 {
 			let s = (x * y).to_string();
 			let l = s.chars().take(s.len() / 2);
 			let r = s.chars().rev().take(s.len() / 2);
