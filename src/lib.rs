8767cffd58ac803a9c14e5bdcb8e17ce7c1fa4bc
Magnus Åsrud <magnusasrud92@gmail.com>
2016-06-11 10:12:13 +0200

    Rust: 11, 12, 14; Python: 13

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![feature(test)]
 extern crate test;
+pub mod primes;
 pub mod ex1;
 pub mod ex2;
 pub mod ex4;
@@ -7,6 +8,9 @@ pub mod ex5;
 pub mod ex8;
 pub mod ex9;
 pub mod ex10;
+pub mod ex11;
+pub mod ex12;
+pub mod ex14;
 
 #[cfg(test)]
 mod tests {
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ extern crate project_euler;
 use project_euler::*;
 
 fn main() {
-	println!("{}", ex2::ex2());
+	println!("{}", ex14::ex14());
 }
