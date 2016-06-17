ec43e878b361777231567340a85e8211b0393d2b
Magnus Åsrud <magnusasrud92@gmail.com>
2016-06-17 22:34:23 +0200

    Fixed python factorials

--- python/ex2.py
+++ python/ex2.py
@@ -1,7 +1,8 @@
 a, b, s = 1, 1, 0
 
 while b < 4000000:
-	a, b = b, b + a
+	b = a + b
+	a, b = b, a
 	if b % 2 == 0:
 		s += b
 
--- python/ex25.py
+++ python/ex25.py
@@ -1,9 +1,8 @@
-a = 1
-b = 1
-idx = 2
+a = b = idx = 1
 
 while b < 10**999:
-	a, b = b, a + b
+	b = a + b
+	a, b = b, a
 	idx += 1
 
 print idx
