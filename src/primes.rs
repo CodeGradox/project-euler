b699890af8c4e13fad2a5d0b469a9fd04579a28e
Magnus Åsrud <magnusasrud92@gmail.com>
2016-07-01 11:16:26 +0200

    Removed slow primes sieve

--- src/primes.rs
+++ src/primes.rs
@@ -1,30 +1,4 @@
 // https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-pub fn primes(n: usize) -> Vec<usize> {
-	if n < 2 {
-		return vec![];
-	}
-	let mut a = vec![true; n];
-
-	for i in (2..n).take_while(|x| x * x < n) {
-		if a[i] {
-			let mut j = i * i;
-			while j < n {
-				a[j] = false;
-				j += i;
-			}
-		}
-	}
-	a[2..]
-		.iter()
-		.zip(2..)
-		.filter_map(|(isprime, num)| if *isprime { Some(num) } else { None })
-		.collect::<Vec<usize>>()
-}
-
-pub fn primes_bit(n: usize) -> Vec<usize> {
-	Primes::new(n).collect()
-}
-
 pub struct Primes {
 	bit_arr: Vec<u8>,
 	max_num: usize,
