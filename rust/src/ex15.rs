8fb3990a512032682ec3b458ac34994b5aef9f26
Magnus Åsrud <magnusasrud92@gmail.com>
2016-08-26 16:09:09 +0200

    Ex15 now uses an arrays instead of vectors

--- rust/src/ex15.rs
+++ rust/src/ex15.rs
@@ -4,12 +4,13 @@
 
 pub fn ex15() {
 	let s = 21; // 20x20 grid
-	let mut v: Vec<Vec<u64>> = vec![vec![1;s]; s];
+	let mut v = [[1u64; 21]; 21];
 
 	for i in 1..s {
 		for j in 1..s {
 			v[i][j] = v[i-1][j] + v[i][j-1];
 		}
 	}
+
 	println!("{}", v[s-1][s-1]);
 }
